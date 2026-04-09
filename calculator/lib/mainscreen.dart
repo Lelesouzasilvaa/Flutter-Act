9e12e2d7cce2736e75cb6ecdcf978dfb14cbdf1c
Leticia de Souza Silva <86673395+Lelesouzasilvaa@users.noreply.github.com>
2026-04-09 16:16:19 -0300

    update 4

--- calculator/lib/mainscreen.dart
+++ calculator/lib/mainscreen.dart
@@ -12,17 +12,25 @@ class MainScreen extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.only(right: 80),
-              child: Icon(Icons.arrow_back_ios, color: const Color.fromARGB(255, 255, 255, 255)),
+              child: Icon(
+                Icons.arrow_back_ios,
+                color: const Color.fromARGB(255, 255, 255, 255),
+              ),
             ),
             SizedBox(
-              child: Text("Regra de Três",
-              style: TextStyle(
-                color: const Color.fromARGB(255, 255, 255, 255)
-              ),),
+              child: Text(
+                "Regra de Três",
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.only(left: 80),
-              child: Icon(Icons.menu, color: const Color.fromARGB(255, 255, 255, 255)),
+              child: Icon(
+                Icons.menu,
+                color: const Color.fromARGB(255, 255, 255, 255),
+              ),
             ),
           ],
         ),
